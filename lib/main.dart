b81ccf38618eb5d9d743b4dfc38d7cda1f34bb5d
Ineza-Agape <i.agape@alustudent.com>
2025-07-29 17:56:25 +0200

    the support and rating page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:safehaven/features/splash/presentation/pages/splash_page.dart';
 import 'package:safehaven/features/auth/presentation/pages/login_page.dart';
 import 'package:safehaven/features/auth/presentation/pages/signup_page.dart';
+   import 'package:safehaven/features/rating/rating_page.dart';
+   import 'package:safehaven/features/support/presentation/pages/support_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,11 +17,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SafeHaven',
-      initialRoute: '/',
+      initialRoute: '/support',
       routes: {
         '/': (context) => const SplashPage(),
         '/login': (context) => const LoginPage(),
-        '/signup': (context) => const SignupPage()
+        '/signup': (context) => const SignupPage(),
+        '/rating': (context) => const RatingPage(),
+        '/support': (context) => const SupportPage(),
       },
     );
   }
