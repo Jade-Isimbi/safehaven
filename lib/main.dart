713712ccad37edb0b8ca32a87523dbb76500e2c7
Jade-Isimbi <j.isimbituz1@alustudent.com>
2025-07-30 09:20:35 +0200

    fix: resolve compilation errors in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:safehaven/features/splash/presentation/pages/splash_page.dart';
 import 'package:safehaven/features/auth/presentation/pages/login_page.dart';
 import 'package:safehaven/features/auth/presentation/pages/signup_page.dart';
-   import 'package:safehaven/features/rating/rating_page.dart';
-   import 'package:safehaven/features/support/presentation/pages/support_page.dart';
+import 'package:safehaven/features/rating/rating_page.dart';
+import 'package:safehaven/features/support/presentation/pages/support_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'features/menu/presentation/pages/menu_page.dart';
@@ -17,35 +17,28 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SafeHaven',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/login': (context) => const LoginPage(),
-        '/signup': (context) => const SignupPage(),
-        '/rating': (context) => const RatingPage(),
-        '/support': (context) => const SupportPage(),
-      },
-      // home: const SplashPage(),
-      debugShowCheckedModeBanner: false,
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => RightsProvider()),
       ],
       child: MaterialApp(
         title: 'SafeHaven',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/login': (context) => const LoginPage(),
+          '/signup': (context) => const SignupPage(),
+          '/rating': (context) => const RatingPage(),
+          '/support': (context) => const SupportPage(),
+        },
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: const MenuPage(),
         debugShowCheckedModeBanner: false,
       ),
     );
   }
-  
 }
