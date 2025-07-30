8940e575cde68d5ec590524147bf349fed686b27
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2025-07-30 20:48:30 +0200

    Routing to main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:safehaven/features/auth/presentation/pages/login_page.dart';
 import 'package:safehaven/features/auth/presentation/pages/signup_page.dart';
 import 'package:safehaven/features/rating/rating_page.dart';
 import 'package:safehaven/features/support/presentation/pages/support_page.dart';
+import 'package:safehaven/features/gbv/presentation/pages/gbv_page.dart'; // ← ADD THIS
+import 'package:safehaven/features/gbv/presentation/pages/helpvictim_page.dart'; // ← ADD THIS
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'features/menu/presentation/pages/menu_page.dart';
@@ -21,9 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => RightsProvider()),
-      ],
+      providers: [ChangeNotifierProvider(create: (_) => RightsProvider())],
       child: MaterialApp(
         title: 'SafeHaven',
         initialRoute: '/',
@@ -34,6 +34,8 @@ class MyApp extends StatelessWidget {
           '/menu': (context) => const MenuPage(),
           '/rating': (context) => const RatingPage(),
           '/support': (context) => const SupportPage(),
+          '/gbv': (context) => const GbvPage(), // ← ADD THIS
+          '/help-victim': (context) => const HelpVictimPage(), // ← ADD THIS
         },
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
