da1bcbee760b17d570ff6d8318d090d7f8b30cdb
Jade-Isimbi <j.isimbituz1@alustudent.com>
2025-07-30 15:02:42 +0200

    Move search functionality to its own page for better UX

--- lib/shared/widgets/custom_bottom_navigation_bar.dart
+++ lib/shared/widgets/custom_bottom_navigation_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../features/menu/presentation/pages/menu_page.dart';
+import '../../features/search/presentation/pages/search_page.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
@@ -24,6 +25,11 @@ class CustomBottomNavigationBar extends StatelessWidget {
           // Navigate to menu page when home icon is tapped
           Navigator.of(context).pushReplacement(
             MaterialPageRoute(builder: (context) => const MenuPage()),
+          );
+        } else if (index == 1) {
+          // Navigate to search page when search icon is tapped
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const SearchPage()),
           );
         }
         // Call the original onTap callback if provided
