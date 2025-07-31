f0e12b69b2c06df55a374c4da40032c52cd21a95
Kellen Murerwa <145454946+kellenmurerwa@users.noreply.github.com>
2025-07-31 07:24:15 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,16 @@ import 'package:safehaven/features/rating/rating_page.dart';
 import 'package:safehaven/features/support/presentation/pages/support_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'features/menu/presentation/pages/menu_page.dart';
 import 'features/rights/presentation/providers/rights_provider.dart';
 import 'features/support/presentation/providers/support_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -33,6 +36,7 @@ class MyApp extends StatelessWidget {
           '/': (context) => const SplashPage(),
           '/login': (context) => const LoginPage(),
           '/signup': (context) => const SignupPage(),
+          '/forgotPassword': (context) => const ForgotPasswordPage(),
           '/menu': (context) => const MenuPage(),
           '/rating': (context) => const RatingPage(),
           '/support': (context) => const SupportPage(),
